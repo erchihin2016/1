84d0e788cfbd685f2b2870b91bc83c9d7d941d40
KonstantinErchihin <erchihin2016@yandex.ru>
2020-08-11 19:22:00 +0500

    исправил 2 задачу

--- src/main/kotlin/hw2.kt
+++ src/main/kotlin/hw2.kt
@@ -1,38 +1,26 @@
 fun main() {
-    print("Вес килограмм: ")
-    val inputWeight = readLine()
-    val weight = tryUserInput2(inputWeight)
-    print("Рост метров: ")
-    val inputHeight = readLine()
-    val height = tryUserInput2(inputHeight)
-
-    val result = weight?.let {
-        if (height != null) {
-            bodyMassIndex(it, height)
-        }
-    }
-    print(result)
+    val index= getBodyMassIndex(1.5, 300)
+    val verdict = getVerdict(index)
+    println(verdict)
 }
 
-fun bodyMassIndex(weight: Float, height: Float): String {
-    val index = weight / (height * height)
-    return when (index) {
-        in 0.0..16.0 -> "Выраженный дифицит массы"
-        in 16.1..18.5 -> "Дифицит массы"
-        in 18.6..25.0 -> "Норма"
-        in 25.1..30.0 -> "Избыточная масса"
-        in 30.1..35.0 -> "Ожирение"
-        in 35.1..40.0 -> "Резкое ожирение"
-        else -> "Очень резкое ожирение"
+fun getVerdict(index : Double): String {
+    return when(index) {
+        in Double.MIN_VALUE..16.0 -> "Выраженный дефицит массы тела."
+        in 16.1..18.5 -> "Недостаточная (дефицит) масса тела."
+        in 18.6..25.0 -> "Норма."
+        in 25.1..30.0 -> "Избыточная масса тела (предожирение)."
+        in 30.1..35.0 -> "Ожирение."
+        in 35.1..40.0 -> "Ожирение резкое."
+        in 40.1..Double.MAX_VALUE -> "Очень резкое ожирение."
+        else -> "Проверьте правильность данных"
     }
 }
 
-
-fun tryUserInput2(input: String?): Float? {
-    try {
-        return input?.toFloat()
-    } catch (e: Exception) {
-        println("Параметры введены неверно!")
-    }
-    return 0.0F
+fun getBodyMassIndex(
+    height: Double,
+    weight: Int
+)
+        : Double {
+    return (weight / (height * height)).toDouble()
 }
